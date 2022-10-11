8420f02671dbd6aaf8fef34846210d243dadf629
Rubel Hossain <rubrex96@hotmail.com>
2022-10-11 07:57:08 +0600

    Initial Router Setup complete

---
+++ src/router/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Blogs from "../components/Blogs/Blogs";
+import Home from "../components/Home/Home";
+import Statistics from "../components/Statistics/Statistics";
+import Topics from "../components/Topics/Topics";
+import Main from "../layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/quiz", element: <Topics /> },
+      { path: "/statistics", element: <Statistics /> },
+      { path: "/blogs", element: <Blogs /> },
+    ],
+  },
+]);
