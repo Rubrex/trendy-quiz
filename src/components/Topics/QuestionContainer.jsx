dcde0800fd5ca3fcc04945ffb508de34b3650e00
Rubel Hossain <rubrex96@hotmail.com>
2022-10-11 15:07:33 +0600

    Correct, Wrong toast alert added

--- src/components/Topics/QuestionContainer.jsx
+++ src/components/Topics/QuestionContainer.jsx
@@ -4,14 +4,13 @@ import { ImEye, ImEyeBlocked } from "react-icons/Im";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const QuestionContainer = ({ ques, index }) => {
+const QuestionContainer = ({ ques, index, score, setScore }) => {
   const { question, options, correctAnswer } = ques;
   // States
-  // const [toast, setToast] = useState("");
+
   // Toast
   const notify = () =>
     toast.info(correctAnswer, {
-      toastId: "info1",
       position: "top-center",
       autoClose: 5000,
       hideProgressBar: false,
@@ -21,7 +20,38 @@ const QuestionContainer = ({ ques, index }) => {
       progress: undefined,
       theme: "colored",
     });
-
+  const correctAnsNotify = (ans) =>
+    toast.success(`Correct: ${ans}`, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  const wrongAnsNotify = (ans) =>
+    toast.error(`Wrong: ${ans}`, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  // Handlers
+  const isCorrect = (option) => {
+    if (option === correctAnswer) {
+      correctAnsNotify(option);
+      setScore((score = score + 1));
+    } else {
+      wrongAnsNotify(option);
+      setScore((score = score - 1));
+    }
+  };
   return (
     <div className="mb-10 neomorph relative">
       <div className="absolute top-6 right-10 cursor-pointer text-2xl">
@@ -31,11 +61,12 @@ const QuestionContainer = ({ ques, index }) => {
       </div>
       <h2 className="py-5 text-2xl font-bold">Quiz: {index}</h2>
       <Interweave className="font-medium py-5" content={question}></Interweave>
-      <div className="grid grid-cols-2 gap-4 w-5/6 mx-auto py-8">
+      <div className="grid grid-col-1 lg:grid-cols-2 gap-4 w-5/6 mx-auto py-8">
         {options.map((option) => (
           <div
             key={option}
-            className="border border-slate-500 py-5 rounded-md border-2 hover:border-primaryColor hover:text-primaryColor cursor-pointer"
+            className=" p-2 border-slate-500 py-5 rounded-md border-2 hover:border-primaryColor hover:text-primaryColor cursor-pointer"
+            onClick={() => isCorrect(option)}
           >
             {option}
           </div>
--- src/components/Topics/Quiz.jsx
+++ src/components/Topics/Quiz.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import QuestionContainer from "./QuestionContainer";
 
 const Quiz = () => {
   const quizInfo = useLoaderData().data;
+  const [score, setScore] = useState(0);
+  console.log(score);
   return (
     <div className="text-center mt-10">
-      <h2 className="text-2xl font-bold text-slate-700 border-l-primaryColor border-l-8 pl-4">
+      <h2 className="text-2xl font-bold text-slate-700 pl-4">
         Quizzes for {quizInfo.name}
       </h2>
 
       <div className="grid grid-cols-4 my-10">
-        <div className="col-span-3 border w-5/6 mx-auto">
+        <div className="col-span-3  w-5/6 mx-auto">
           {/* Iterate Questions Here */}
 
           {quizInfo.questions.map((question, index) => {
@@ -21,10 +23,13 @@ const Quiz = () => {
                 key={question.id}
                 index={index}
                 ques={question}
+                score={score}
+                setScore={setScore}
               />
             );
           })}
         </div>
+        {/* Quiz Score  */}
         <div className="border"></div>
       </div>
     </div>
