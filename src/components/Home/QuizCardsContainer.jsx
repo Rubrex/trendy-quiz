9c0faa60d2c6a44a467a5ab300d994c8b41c8c42
Rubel Hossain <rubrex96@hotmail.com>
2022-10-11 09:10:09 +0600

    Header quiz section added

--- src/components/Home/QuizCardsContainer.jsx
+++ src/components/Home/QuizCardsContainer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Topics from "../Topics/Topics";
 
 const QuizCardsContainer = () => {
-  return <div className="container mx-auto my-10"></div>;
+  return (
+    <div className="container mx-auto my-10">
+      <h2 className="text-3xl font-medium border-l-primaryColor border-l-8 pl-4 ">
+        Our Topics :
+      </h2>
+      <div className="mt-10">
+        <Topics></Topics>
+      </div>
+    </div>
+  );
 };
 
 export default QuizCardsContainer;
